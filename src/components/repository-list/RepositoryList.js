bbad428f4b43d9b06d4cab6c7a568c6077f8b5ce
m.stikleris <mantas.stikleris@adform.com>
2019-05-05 00:14:12 +0300

    Add search query reducer, test it's behavior using `useEffect` hook

--- src/components/repository-list/RepositoryList.js
+++ src/components/repository-list/RepositoryList.js
@@ -1,17 +1,36 @@
-import React, { useRef } from 'react';
+import React, { useRef, useReducer, useEffect } from 'react';
 import './RepositoryList.scss';
 import {FontAwesomeIcon as Icon} from '@fortawesome/react-fontawesome';
 import {faSearch} from '@fortawesome/free-solid-svg-icons';
 
+const reducer = (state, action) => {
+    switch(action.type) {
+        case 'SET_SEARCH_QUERY':
+            return {
+            ...state,
+            searchQuery: action.searchQuery
+        };
+        default:
+            return state
+    }
+};
+
 const RepositoryList = () => {
     const searchRef = useRef('');
+    const [state, dispatch] = useReducer(reducer, {
+       searchQuery: ''
+    });
+
+    useEffect(() => {
+        console.log(state.searchQuery);
+    }, [state]);
 
     return (
         <div className="RepositoryList">
             <div className="SearchContainer">
                 <div className="Search">
                     <Icon icon={faSearch}/>
-                    <input placeholder="Search" ref={searchRef}/>
+                    <input placeholder="Search" ref={searchRef} onChange={() => dispatch({type: 'SET_SEARCH_QUERY', searchQuery: searchRef.current.value})}/>
                 </div>
             </div>
         </div>
