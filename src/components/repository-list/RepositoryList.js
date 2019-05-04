bcec1c5ac2e82785862269a90570d010efff3ebc
m.stikleris <mantas.stikleris@adform.com>
2019-05-05 00:05:35 +0300

    Add search icon and styles

--- src/components/repository-list/RepositoryList.js
+++ src/components/repository-list/RepositoryList.js
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react';
 import './RepositoryList.scss';
+import {FontAwesomeIcon as Icon} from '@fortawesome/react-fontawesome';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
 
 const RepositoryList = () => {
     const searchRef = useRef('');
@@ -8,6 +10,7 @@ const RepositoryList = () => {
         <div className="RepositoryList">
             <div className="SearchContainer">
                 <div className="Search">
+                    <Icon icon={faSearch}/>
                     <input placeholder="Search" ref={searchRef}/>
                 </div>
             </div>
