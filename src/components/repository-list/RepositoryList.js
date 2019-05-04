532bfabba8c0b8aa9bf82677fcfe93bef99eb5b8
m.stikleris <mantas.stikleris@adform.com>
2019-05-04 23:58:57 +0300

    Hook input with `useRef`

--- src/components/repository-list/RepositoryList.js
+++ src/components/repository-list/RepositoryList.js
@@ -2,12 +2,13 @@ import React, { useRef } from 'react';
 import './RepositoryList.scss';
 
 const RepositoryList = () => {
+    const searchRef = useRef('');
 
     return (
         <div className="RepositoryList">
             <div className="SearchContainer">
                 <div className="Search">
-                    <input placeholder="Search"/>
+                    <input placeholder="Search" ref={searchRef}/>
                 </div>
             </div>
         </div>
