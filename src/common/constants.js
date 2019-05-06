b6a6001aeb9938a95905337ccc2df399047bacde
m.stikleris <mantas.stikleris@adform.com>
2019-05-06 23:44:24 +0300

    Send empty authorization token if it's not defined in `.env`

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,9 +1,11 @@
 export const DEBOUNCE_DELAY = 500;
 export const ITEMS_PER_PAGE = 5;
 
+const GIT_AUTHORIZATION_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
 export const GIT_AUTHORIZATION_HEADER = {
     headers: {
-        Authorization: `token ${ process.env.REACT_APP_GITHUB_TOKEN }`
+        Authorization: GIT_AUTHORIZATION_TOKEN ? `token ${ GIT_AUTHORIZATION_TOKEN }` : ''
     }
 };
 
